6d216961943ca10f09e1dbd382f8ff40efa4e82c
Elaine Demetrion <esdemetrion@gmail.com>
2023-12-09 00:14:16 -0500

    Updates 10 years bash script and file save script to use new topics by year/month

--- run_ten_years.sh
+++ run_ten_years.sh
@@ -2,53 +2,87 @@
 
 # immediately exit if there's an error
 set -e
-
-# TODO: are we running all of the times at once? Or one month at a time?
-
-# collectors: rrc10, rrc00, route-views2, route-views.linx
 collectors=("rrc10", "rrc00", "route-views2", "route-views.linx")
-start_time='2013-11-01T00:00:00'
-end_time='2023-11-01T00:00:00'
+start_year = 2013
+start_month = 1
+end_year = 2023
+end_month = 12
+collector = "rrc00"
 
-
-# Load ribs/updates data into the kafka topics for all collectors
-for collector in ${collectors[@]}; do
-  KAFKA_HOST=localhost:9092 python produce_bgpdata.py -t ribs --collector $collector --startTime $start_time --endTime $end_time
-  KAFKA_HOST=localhost:9092 python produce_bgpdata.py -t updates --collector $collector --startTime $start_time --endTime $end_time
-done
-
-# Produce bgpatom and weighted_atom for all of the collectors
-for collector in ${collectors[@]}; do
-  python3 produce_bgpatom.py -c $collector -s $start_time -e $end_time
-  python3 produce_weightedatom.py -c $collector -s $start_time -e $end_time
-done
-
-# Produce bc_score for all of the collectors
-for collector in ${collectors[@]}; do
-  python3 produce_bcscore.py -c $collector -s $start_time -e $end_time
-done
-
-# Run everything for each year and month
-# TODO: is this remotely close to the correct syntax?
-start_year=2013
-end_year=2023
-for month in {01..12}; do
-  for year in {start_year..end_year}; do
-    start_time="$year-$month-01T00:00:00"
-    end_month=((month+1))
-    end_year = year
-    if [ $((end_month == 13)) ]; then
-      end_month=01
-      end_year=((year+1))
+for month in {1..12}; do
+  for year in {$start_year..$end_year}; do
+    if [[ $year -eq $end_year && $month -gt $end_month]]; then
+      continue
     fi
-    end_time="$end_year-$end_month-01T00:00:00"
+    # Load the ribs/updates data into the kafka topics for one collector
+    KAFKA_HOST=localhost:9092 python produce_bgpdata.py -t ribs --collector $collector --year $year --month $month
+    KAFKA_HOST=localhost:9092 python produce_bgpdata.py -t updates --collector $collector --year $year --month $month
+
+    # Produce bgpatom and weighted_atom for one collector
+    python3 produce_bgpatom.py -c $collector --year $year --month $month
+    python3 produce_weightedatom.py -c $collector --year $year --month $month
+
+    # Produce bc_score for all of the collectors
+    python3 produce_bcscore.py -c $collector --year $year --month $month
 
     # Calculate the hegemony scores
-    python3 produce_hege.py -s $start_time -e $end_time -c rrc00,rrc10,route-views.linx,route-views2
+    python3 produce_hege.py --year $year --month $month -c $collector
 
-    # download the hege scores for this time period
+    # download the hege scores for this time period to a csv file
+    python3 save_hege_scores.py --year $year --month $month
   done
 done
+
+
+
+
+
+## TODO: are we running all of the times at once? Or one month at a time?
+#
+## collectors: rrc10, rrc00, route-views2, route-views.linx
+#collectors=("rrc10", "rrc00", "route-views2", "route-views.linx")
+#start_time='2013-11-01T00:00:00'
+#end_time='2023-11-01T00:00:00'
+#
+#
+## Load ribs/updates data into the kafka topics for all collectors
+#for collector in ${collectors[@]}; do
+#  KAFKA_HOST=localhost:9092 python produce_bgpdata.py -t ribs --collector $collector --startTime $start_time --endTime $end_time
+#  KAFKA_HOST=localhost:9092 python produce_bgpdata.py -t updates --collector $collector --startTime $start_time --endTime $end_time
+#done
+#
+## Produce bgpatom and weighted_atom for all of the collectors
+#for collector in ${collectors[@]}; do
+#  python3 produce_bgpatom.py -c $collector -s $start_time -e $end_time
+#  python3 produce_weightedatom.py -c $collector -s $start_time -e $end_time
+#done
+#
+## Produce bc_score for all of the collectors
+#for collector in ${collectors[@]}; do
+#  python3 produce_bcscore.py -c $collector -s $start_time -e $end_time
+#done
+#
+## Run everything for each year and month
+## TODO: is this remotely close to the correct syntax?
+#start_year=2013
+#end_year=2023
+#for month in {01..12}; do
+#  for year in {start_year..end_year}; do
+#    start_time="$year-$month-01T00:00:00"
+#    end_month=((month+1))
+#    end_year = year
+#    if [ $((end_month == 13)) ]; then
+#      end_month=01
+#      end_year=((year+1))
+#    fi
+#    end_time="$end_year-$end_month-01T00:00:00"
+#
+#    # Calculate the hegemony scores
+#    python3 produce_hege.py -s $start_time -e $end_time -c rrc00,rrc10,route-views.linx,route-views2
+#
+#    # download the hege scores for this time period
+#  done
+#done
 
 
 # run
---
+++ save_hege_scores.py
@@ -0,0 +1,75 @@
+import sys
+import msgpack
+import argparse
+import csv
+import os
+
+from confluent_kafka import Consumer, TopicPartition, KafkaError
+
+"""
+This should copy all of the hegemony scores from the topic ihr_hegemony_yyyy_mm to a file called hegescores/hegePerASN_yyyy-mm.csv
+"""
+
+# populates a csv file '.../hegePerASN_yyyy-mm.csv' with a list of {asn, hege} values
+def downloadDataToFile(server, num_partitions, month, year, datapath):
+    topic = "ihr_hegemony_"+year+"_"+month
+    filename = datapath + "/" "hegePerASN" + "_" + args.year + "-" + month + ".csv"
+
+    consumer = Consumer({
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'ihr_tail',  # does this need to be named this?
+        'enable.auto.commit': False,
+    })
+
+    partition = TopicPartition(topic, num_partitions)
+    low, high = consumer.get_watermark_offsets(partition)
+    partition = TopicPartition(topic, num_partitions, low)
+    consumer.assign([partition])
+
+    with open(filename, 'a', newline='') as file:
+        file.truncate(0)  # delete old contents
+        writer = csv.writer(file)
+        i = 0
+        while True:
+            msg = consumer.poll(1000)
+            msgdict = {
+                'topic': msg.topic(),
+                'partition': msg.partition(),
+                'key': msg.key(),
+                'timestamp': msg.timestamp(),
+                'headers': msg.headers(),
+                'value': msgpack.unpackb(msg.value(), raw=False)
+            }
+
+
+            data = msgdict['value']
+            writer.writerow([data['asn'], data['hege']])
+
+            i += 1
+            if i >= high:
+                break
+
+        consumer.close()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--server', default='localhost:9092')
+    parser.add_argument('-p', '--partitions', default=0)
+    parser.add_argument('-m', '--month', default='2020')
+    parser.add_argument('-y', '--year', default='1')
+
+    args = parser.parse_args()
+    monthStr = str(args.month)
+    if len(monthStr) < 2:
+        monthStr = '0' + monthStr
+
+    datapath = "./hegescores"
+    # create folder for hegemony data
+    dataFolder = os.path.exists(datapath)
+    if not dataFolder:
+        os.mkdir(datapath)
+
+
+    # searchByASNKafka(args.asn, args.topic, args.partitions, args.server)
+    downloadDataToFile(args.server, args.partitions, monthStr, str(args.year), datapath)
